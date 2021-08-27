570854459c24753645bfd68094a0e5f9bf22c001
Orikami <65590892+ORIKAMI-NEP@users.noreply.github.com>
2021-08-27 14:16:19 +0900

    Update src/quiz/Description.jsx
    Co-authored-by: Naoki Takahashi <43571743+nemuki@users.noreply.github.com>

--- src/quiz/Description.jsx
+++ src/quiz/Description.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
 import "./css/style.css";
 import "./css/Description.css";
 import Timer from "../common/Timer";
