08026abac905dec85fce0feeb53884d9bc45855a
Togo555-mp4 <5ting20X@gmail.com>
2021-09-05 20:19:03 +0900

    アッパーキャメルに

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Test from "./Test"; // TODO: 完成後消去
 import Explanation from "./learn/Explanation";
-import learnResult from "./learn/Result";
+import LearnResult from "./learn/Result";
 import Description from "./quiz/Description";
 import Answer from "./quiz/Answer";
-import quizResult from "./quiz/Result";
+import QuizResult from "./quiz/Result";
 import Voting from "./quiz/Voting";
 import Award from "./quiz/Award";
 
@@ -14,10 +14,10 @@ export default function App() {
     <BrowserRouter>
       <Route path="/" exact component={Test} /> {/* TODO: 完成後消去 */}
       <Route path="/explanation" exact component={Explanation} />
-      <Route path="/result" exact component={learnResult} />
+      <Route path="/result" exact component={LearnResult} />
       <Route path="/description" exact component={Description} />
       <Route path="/answer" exact component={Answer} />
-      <Route path="/result" exact component={quizResult} />
+      <Route path="/result" exact component={QuizResult} />
       <Route path="/voting" exact component={Voting} />
       <Route path="/award" exact component={Award} />
     </BrowserRouter>
