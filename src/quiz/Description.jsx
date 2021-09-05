056d8272e46a8de5ddcb15b5e16f7284ee8b84cc
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-09-06 00:03:52 +0900

    quiz.cssに変更

--- src/quiz/Description.jsx
+++ src/quiz/Description.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import "./css/style.css";
+import "./css/quiz.css";
 import "./css/Description.css";
 import Timer from "../common/Timer";
 
