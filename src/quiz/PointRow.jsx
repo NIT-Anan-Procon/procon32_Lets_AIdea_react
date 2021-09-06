0c908c8ff927f1b43464b872f5bd78a105d36e06
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-09-06 19:20:47 +0900

    PointCellコンポーネントを作成 #69

--- src/quiz/PointRow.jsx
+++ src/quiz/PointRow.jsx
@@ -2,33 +2,29 @@ import React from "react";
 import "./css/PointRow.css";
 import Icon from "../common/Icon";
 import Name from "../common/Name";
+import PointCell from "./PointCell";
 
 export default function PointRow(props) {
-  const rankOption = [
-    ["firstPlayer", "1st"],
-    ["secondPlayer", "2nd"],
-    ["thirdPlayer", "3rd"],
-  ];
   return (
-    <div id={rankOption[props.number - 1][0]}>
-      <div className="rank">{rankOption[props.number - 1][1]}</div>
+    <div id={props.id}>
+      <div className="rank">{props.rank}</div>
       <Icon src={props.icon} />
       <Name text={props.name} />
-      <div className="descriptionPointBox">
-        説明
-        <p>{props.descriptionPoint}[pt]</p>
-      </div>
-      <div className="answerPointBox">
-        回答
-        <p>{props.answerPoint}[pt]</p>
-      </div>
-      <div className="totalPointBox">
-        合計
-        <p>
-          {props.descriptionPoint + props.answerPoint}
-          [pt]
-        </p>
-      </div>
+      <PointCell
+        class="descriptionPointBox"
+        title="説明"
+        point={props.descriptionPoint}
+      />
+      <PointCell
+        class="answerPointBox"
+        title="回答"
+        point={props.answerPoint}
+      />
+      <PointCell
+        class="totalPointBox"
+        title="合計"
+        point={props.descriptionPoint + props.answerPoint}
+      />
     </div>
   );
 }
