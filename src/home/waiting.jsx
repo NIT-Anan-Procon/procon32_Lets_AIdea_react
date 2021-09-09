aaae053096d01b8752d558a8b3f904643d0349b5
Maoki55 <1187589@st.anan-nct.ac.jp>
2021-09-09 15:52:49 +0900

    画面構成要素を追加

--- src/home/waiting.jsx
+++ src/home/waiting.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "./css/waiting.css";
 import axios from "axios";
 
 export default function waiting() {
+  // const [roomId, setRoomId] = useState("123456789");
+
   const copyText = () => {
-    navigator.clipboard.writeText("Copy success!").then((r) => "Copy failed.");
+    navigator.clipboard.writeText("123456789").then((r) => "Copy failed.");
   };
 
   return (
     <div id="waiting">
-      <text>部屋ID</text>
-      <button id="copyButton" onClick={copyText} value="コピー" />
+      <div id="room">部屋ID</div>
+      <div id="roomId">123456789</div>
+      <button id="copyButton" onClick={copyText}>
+        コピーする
+      </button>
+      <div id="user">
+        <div id="icon">アイコン</div>
+        <div id="message">888が入室しました</div>
+      </div>
+      <button id="startButton">ゲームを始める</button>
     </div>
   );
 }
