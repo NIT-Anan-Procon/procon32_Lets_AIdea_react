f93c9b296b5894ed5e12b5d0ead583d733179870
Togo555-mp4 <5ting20X@gmail.com>
2021-09-10 13:08:41 +0900

    APIテスト

--- src/quiz/pages/Award.jsx
+++ src/quiz/pages/Award.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 import "./css/Award.css";
 import Title from "../../common/components/Title";
 import Image from "../../common/components/Image";
@@ -9,22 +10,30 @@ import OtherDescription from "../../common/components/OtherDescription";
 import NgWord from "../../common/components/NgWord";
 
 export default function Award() {
-  const data = {
-    name: "Togo",
-    icon: "https://source.unsplash.com/featured/?dog",
-    explanation: "百獣の王は静かに微笑みを湛えている",
-    ng: ["ライオン", "草原"],
-    pictureURL: "https://source.unsplash.com/featured/?random",
-  };
+  const [data, setData] = useState();
   const [ngWord, setNgWord] = useState("");
   const history = useHistory();
 
   useEffect(() => {
-    for (let i = 0; i < data.ng.length; i++) {
-      setNgWord((ngWord) => ngWord + data.ng[i]);
-      if (i !== data.ng.length - 1) setNgWord((ngWord) => ngWord + ", ");
-    }
+    axios
+      .get("http://localhost/API/End.php", {})
+      .then((res) => {
+        console.log(res);
+        setData(res);
+        getNgWord(res);
+      })
+      .catch((error) => {
+        console.log(error.request.status);
+      });
   }, []);
+
+  const getNgWord = (data) => {
+    console.log(data.data.ng.length);
+    for (let i = 0; i < data.data.ng.length; i++) {
+      setNgWord((ngWord) => ngWord + data.data.ng[i]);
+      if (i !== data.data.ng.length - 1) setNgWord((ngWord) => ngWord + ", ");
+    }
+  };
 
   const againHandleSubmit = () => {
     // TODO 待機画面に戻るパスを追加
@@ -36,15 +45,16 @@ export default function Award() {
     history.push("");
   };
 
+  console.log(data);
   return (
     <div id="award">
       <Title text="優秀作品" />
-      <Image src={data.pictureURL} alt="picture" />
+      <Image src={data.data.pictureURL} alt="picture" />
       <div className="player">
-        <Icon src={data.icon} />
-        <Name text={data.name} />
+        <Icon src={data.data.icon} />
+        <Name text={data.data.name} />
         <NgWord text={ngWord} />
-        <OtherDescription title="説明文" text={data.explanation} />
+        <OtherDescription title="説明文" text={data.data.explanation} />
       </div>
       <form className="buttonForm">
         <input
