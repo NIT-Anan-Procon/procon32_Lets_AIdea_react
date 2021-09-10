dd4f022f8b070fb0d0ec04109825afde0da21db4
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-09-10 12:03:38 +0900

    API通信 #108

--- src/quiz/pages/Result.jsx
+++ src/quiz/pages/Result.jsx
@@ -6,88 +6,54 @@ import Title from "../../common/components/Title";
 import PointRow from "../components/PointRow";
 
 export default function Result() {
-  // const history = useHistory();
   const [data, setData] = useState();
-
-  const params = new FormData();
-  params.append("name", "orikami");
-  params.append("password", "0514");
-  // useEffect(() => {
-  //   axios
-  //     .post("http://localhost/API/User/Login.php", params, {
-  //       headers: {
-  //         "content-type": "multipart/form-data",
-  //       },
-  //     })
-  //     .then((result) => {
-  //       console.log(result.status);
-  //       console.log(result);
-  //       setData(result);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.request.status);
-  //     });
-  // }, []);
+  const history = useHistory();
 
   useEffect(() => {
-    // GetQuizResult
-    axios
-      .get("http://localhost/API/Quiz/GetQuizResult.php", {
-        withCredentials: true,
-      })
-      .then((result) => {
-        console.log(result);
-        setData(result);
-      });
+    axios.get("http://localhost/API/Quiz/GetQuizResult.php").then((result) => {
+      console.log(result.data);
+      setData(result.data);
+    });
   }, []);
 
-  // useEffect(() => { // GetUserInfo
-  //     axios
-  //         .get("http://localhost/API/User/GetUserInfo.php", {
-  //           withCredentials: true,
-  //         })
-  //         .then(result => {
-  //           console.log(result);
-  //           setData(result);
-  //         })
-  // }, []);
+  const handleSubmit = () => {
+    history.push("/quiz/voting");
+  };
 
-  // const handleSubmit = () => {
-  //   history.push("/quiz/voting");
-  // };
+  if (!data) return null;
 
   return (
     <div id="result">
-      {/*<Title text="結果発表" />*/}
-      {/*<div id="pointTable">*/}
-      {/*  <PointRow*/}
-      {/*    id="firstPlayer"*/}
-      {/*    rank="1st"*/}
-      {/*    icon="https://source.unsplash.com/featured/?random"*/}
-      {/*    name="Togo"*/}
-      {/*    descriptionPoint={data.playerId[0].descriptionPoint}*/}
-      {/*    answerPoint={data.playerId[0].answerPoint}*/}
-      {/*  />*/}
-      {/*  <PointRow*/}
-      {/*    id="secondPlayer"*/}
-      {/*    rank="2nd"*/}
-      {/*    icon="https://source.unsplash.com/featured/?random"*/}
-      {/*    name="Taiki"*/}
-      {/*    descriptionPoint={data.playerId[1].descriptionPoint}*/}
-      {/*    answerPoint={data.playerId[1].answerPoint}*/}
-      {/*  />*/}
-      {/*  <PointRow*/}
-      {/*    id="thirdPlayer"*/}
-      {/*    rank="3rd"*/}
-      {/*    icon="https://source.unsplash.com/featured/?random"*/}
-      {/*    name="Ibuki"*/}
-      {/*    descriptionPoint={data.playerId[2].descriptionPoint}*/}
-      {/*    answerPoint={data.playerId[2].answerPoint}*/}
-      {/*  />*/}
-      {/*</div>*/}
-      {/*<form onSubmit={handleSubmit} className="buttonForm">*/}
-      {/*  <input type="submit" value="投票へ" />*/}
-      {/*</form>*/}
+      <Title text="結果発表" />
+      <div id="pointTable">
+        <PointRow
+          id="firstPlayer"
+          rank="1st"
+          icon={data[0].icon}
+          name={data[0].name}
+          descriptionPoint={data[0].exp}
+          answerPoint={data[0].ans}
+        />
+        <PointRow
+          id="secondPlayer"
+          rank="2nd"
+          icon={data[1].icon}
+          name={data[1].name}
+          descriptionPoint={data[1].exp}
+          answerPoint={data[1].ans}
+        />
+        <PointRow
+          id="thirdPlayer"
+          rank="3rd"
+          icon={data[2].icon}
+          name={data[2].name}
+          descriptionPoint={data[2].exp}
+          answerPoint={data[2].ans}
+        />
+      </div>
+      <form onSubmit={handleSubmit} className="buttonForm">
+        <input type="submit" value="投票へ" />
+      </form>
     </div>
   );
 }
