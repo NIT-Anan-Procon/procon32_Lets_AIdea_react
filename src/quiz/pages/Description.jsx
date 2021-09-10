ea8ea201179c01c3767da3cd4ba4c6ff00b771af
Togo555-mp4 <5ting20X@gmail.com>
2021-09-10 12:27:47 +0900

    NGワード仕様変更

--- src/quiz/pages/Description.jsx
+++ src/quiz/pages/Description.jsx
@@ -13,35 +13,6 @@ import TimeUp from "../../common/TimeUp";
 
 export default function Description() {
   const [data, setData] = useState();
-  // const [data, setData] = useState({
-  //     playerId: [
-  //         {
-  //             ng: ["草原", "ライオン"],
-  //             AI: "草原でライオンが座っています",
-  //             pictureURl: "https://source.unsplash.com/featured/?",
-  //             gamemode: null,
-  //         },
-  //         {
-  //             ng: ["草", "赤い", "光"],
-  //             AI: "草に囲まれた赤い建物に光が当たっています",
-  //             pictureURl: "",
-  //             gamemode: null,
-  //         },
-  //         {
-  //             ng: ["NG1", "NG2", "NG3"],
-  //             AI: "AI説明文",
-  //             pictureURl: "",
-  //             gamemode: null,
-  //         },
-  //         {
-  //             ng: ["NG1", "NG2", "NG3"],
-  //             AI: "AI説明文",
-  //             pictureURl: "",
-  //             gamemode: null,
-  //         },
-  //     ],
-  // });
-  const params = new FormData();
   const [ngWord, setNgWord] = useState("");
   const [attentionMessage, setAttentionMessage] = useState("");
   const [myDescription, setMyDescription] = useState("");
@@ -49,57 +20,26 @@ export default function Description() {
   const [time, setTime] = useState(30);
   const timer = useRef(null);
 
-  // useEffect(() => {
-  //     axios
-  //         .post("http://localhost/API/User/Login.php", params, {
-  //             headers: {
-  //                 "content-type": "multipart/form-data",
-  //             },
-  //         })
-  //         .then((login) => {
-  //             console.log(login.status);
-  //             console.log(login);
-  //             //setData(login);
-  //             getStart();
-  //         })
-  //         .catch((error) => {
-  //             console.log(error.request.status);
-  //         });
-  // }, []);
-  //
-  // function getStart() {
-  //     // GetStart
-  //     axios
-  //         .get("http://localhost/API/Start.php", {
-  //             withCredentials: true,
-  //         })
-  //         .then((description) => {
-  //             console.log(description);
-  //             setData(description);
-  //             getNgWord();
-  //         });
-  // }
   useEffect(() => {
     axios
-      .get("http://localhost/API/Start.php", {
-        withCredentials: true,
-      })
-      .then((description) => {
-        console.log(description);
-        setData(description);
-        getNgWord();
+      .get("http://localhost/API/Start.php", {})
+      .then((res) => {
+        console.log(res);
+        setData(res);
+        getNgWord(res);
       })
       .catch((error) => {
         console.log(error.request.status);
       });
   }, []);
 
-  function getNgWord() {
-    for (let i = 0; i < data.ng.length; i++) {
-      setNgWord((ngWord) => ngWord + data.ng[i]);
-      if (i !== data.ng.length - 1) setNgWord((ngWord) => ngWord + ", ");
+  const getNgWord = (data) => {
+    console.log(data.data.ng.length);
+    for (let i = 0; i < data.data.ng.length; i++) {
+      setNgWord((ngWord) => ngWord + data.data.ng[i]);
+      if (i !== data.data.ng.length - 1) setNgWord((ngWord) => ngWord + ", ");
     }
-  }
+  };
 
   // useEffect(() => {
   //     // TODO: APIとの通信
@@ -142,30 +82,25 @@ export default function Description() {
     }, 5000);
   }
 
+  if (!data) return null;
   return (
     <div id="description">
-      {/*<Title text="この画像を説明しよう"/>*/}
-      {/*<Image*/}
-      {/*    src={data.pictureURL}*/}
-      {/*    alt="問題の画像"*/}
-      {/*/>*/}
-      {/*<OtherDescription*/}
-      {/*    title="AIの説明文"*/}
-      {/*    text={data.AI}*/}
-      {/*/>*/}
-      {/*<NgWord text={ngWord}/>*/}
-      {/*<form onSubmit={handleSubmit} id="descriptionForm">*/}
-      {/*    <AttentionMessage text={attentionMessage}/>*/}
-      {/*    <input*/}
-      {/*        type="text"*/}
-      {/*        value={myDescription}*/}
-      {/*        onChange={handleChange}*/}
-      {/*        className="textBox"*/}
-      {/*    />*/}
-      {/*    <input type="submit" value="送信"/>*/}
-      {/*</form>*/}
-      {/*<Timer time={time}/>*/}
-      {/*<TimeUp time={time}/>*/}
+      <Title text="この画像を説明しよう" />
+      <Image src={data.data.pictureURL} alt="問題の画像" />
+      <OtherDescription title="AIの説明文" text={data.data.AI} />
+      <NgWord text={ngWord} />
+      <form onSubmit={handleSubmit} id="descriptionForm">
+        <AttentionMessage text={attentionMessage} />
+        <input
+          type="text"
+          value={myDescription}
+          onChange={handleChange}
+          className="textBox"
+        />
+        <input type="submit" value="送信" />
+      </form>
+      <Timer time={time} />
+      <TimeUp time={time} />
     </div>
   );
 }
