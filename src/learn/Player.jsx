e7910b0f0b778586220ea8568e3a7d947a57236f
Togo555-mp4 <5ting20X@gmail.com>
2021-09-12 14:10:12 +0900

    クラス名の変更

--- src/learn/Player.jsx
+++ src/learn/Player.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./css/Player.css";
+import Icon from "../common/components/Icon";
+import Name from "../common/components/Name";
 
 export default function Player(props) {
   return (
     <div className="player">
-      <div className="playerImg">
-        <img src={props.src} alt="playerImg" />
-        <div className="playerName">{props.name}</div>
-      </div>
-      <div className="balloon playerExplanation">
+      <Icon src={props.src} alt="playerImg" />
+      <Name text={props.name} />
+      <div className="balloon">
         <p>{props.explanation}</p>
       </div>
     </div>
