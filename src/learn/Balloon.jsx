1d7b9682f6ecfa21b695f28cbe8719131089e2ee
Togo555-mp4 <5ting20X@gmail.com>
2021-09-13 12:36:38 +0900

    スクロールできるようにした

--- src/learn/Balloon.jsx
+++ src/learn/Balloon.jsx
@@ -1,6 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./css/Balloon.css";
 
 export default function Balloon(props) {
-  return <div className="balloon">{props.text}</div>;
+  return (
+    <div className="balloon">
+      <div className="scroll">
+        <p>{props.text}</p>
+      </div>
+    </div>
+  );
 }
