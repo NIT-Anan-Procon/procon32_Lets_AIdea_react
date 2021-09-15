27f9d6d9b23e0baca695add318e08b7d95c602d0
Maoki55 <1187589@st.anan-nct.ac.jp>
2021-09-15 14:54:10 +0900

    テキスト・ボタンの配置

--- src/common/pages/Selection.jsx
+++ src/common/pages/Selection.jsx
@@ -4,5 +4,14 @@ import "./css/Selection.css";
 import axios from "axios";
 
 export default function Selection() {
-  return <div>モード選択</div>;
+  return (
+    <div id="selection">
+      <div className="textRoom">部屋を作る</div>
+      <button id="learnButton">学習モード</button>
+      <button id="quizButton">クイズモード</button>
+      <div className="textRoom">部屋に入る</div>
+      <button id="joinButton">参加</button>
+      <button>ライブラリ</button>
+    </div>
+  );
 }
