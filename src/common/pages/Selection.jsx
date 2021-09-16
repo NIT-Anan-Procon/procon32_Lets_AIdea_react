5dcb592b25b352c89e25f924667224a35c62a64c
Maoki55 <1187589@st.anan-nct.ac.jp>
2021-09-16 16:38:55 +0900

    POSTのエラーが発生している

--- src/common/pages/Selection.jsx
+++ src/common/pages/Selection.jsx
@@ -4,7 +4,9 @@ import "./css/Selection.css";
 import axios from "axios";
 
 export default function Selection() {
+  const [gameMode, setGameMode] = useState("");
   const history = useHistory();
+  const params = new FormData();
 
   useEffect(() => {
     axios
@@ -23,6 +25,60 @@ export default function Selection() {
       });
   }, []);
 
+  const makeLearnRoom = () => {
+    // setGameMode("0000");
+    // console.log(gameMode);
+    // params.append("gamemode", gameMode);
+    axios
+      .post(
+        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/Room/CreateRoom.php",
+        {
+          gamemode: "0000",
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "content-type": "multipart/form-data",
+          },
+        }
+      )
+      .then((result) => {
+        console.log("--- 学習モード部屋の作成に成功 ---");
+        console.log("レスポンス: " + result.status);
+        // history.push("/waiting");
+      })
+      .catch((error) => {
+        console.log("--- 学習モード部屋でエラーが発生 ---");
+        console.log("レスポンス: " + error.request.status);
+      });
+  };
+
+  const makeQuizRoom = () => {
+    setGameMode("1100");
+    console.log(gameMode);
+    params.append("gamemode", gameMode);
+    axios
+      .post(
+        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/Room/CreateRoom.php",
+        params,
+        {
+          withCredentials: true,
+          headers: {
+            "content-type": "multipart/form-data",
+          },
+        }
+      )
+      .then((result) => {
+        console.log("--- クイズモード部屋の作成に成功 ---");
+        console.log("レスポンス: " + result.status);
+        // history.push("/waiting");
+      })
+      .catch((error) => {
+        console.log("--- クイズモード部屋でエラーが発生 ---");
+        console.log("レスポンス: " + error.request.status);
+      });
+  };
+
   const moveToLibrary = () => {
     history.push("/library");
   };
@@ -30,8 +86,12 @@ export default function Selection() {
   return (
     <div id="selection">
       <div id="textMakeRoom">部屋を作る</div>
-      <button id="learnButton">学習モード</button>
-      <button id="quizButton">クイズモード</button>
+      <button id="learnButton" onClick={makeLearnRoom}>
+        学習モード
+      </button>
+      <button id="quizButton" onClick={makeQuizRoom}>
+        クイズモード
+      </button>
       <input type="text" placeholder="部屋IDを入力してね" id="roomId" />
       <div id="textJoinRoom">部屋に入る</div>
       <button id="joinButton">参加</button>
