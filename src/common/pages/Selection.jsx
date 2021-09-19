4865d9478e6578857014742b4b338cf834acae6b
Maoki55 <1187589@st.anan-nct.ac.jp>
2021-09-19 22:48:36 +0900

    Selection.jsxの不要な表現を削除

--- src/common/pages/Selection.jsx
+++ src/common/pages/Selection.jsx
@@ -12,14 +12,10 @@ export default function Selection() {
 
   useEffect(() => {
     axios
-      .get(
-        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/User/GetUserInfo.php",
-        {
-          withCredentials: true,
-        }
-      )
+      .get("http://localhost/API/User/GetUserInfo.php", {
+        withCredentials: true,
+      })
       .then((res) => {
-        console.log("--- GetUserInfo ---");
         console.log(res);
       })
       .catch((error) => {
@@ -29,19 +25,14 @@ export default function Selection() {
 
   const doLogout = () => {
     axios
-      .get(
-        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/User/Logout.php",
-        {
-          withCredentials: true,
-        }
-      )
+      .get("http://localhost/API/User/Logout.php", {
+        withCredentials: true,
+      })
       .then((res) => {
-        console.log("--- ログアウトしました ---");
         console.log(res.request.status);
         history.push("/login");
       })
       .catch((error) => {
-        console.log("--- ログアウトに失敗しました ---");
         console.log(error.request.status);
       });
   };
@@ -49,50 +40,38 @@ export default function Selection() {
   const makeLearnRoom = () => {
     params.append("gamemode", learnMode);
     axios
-      .post(
-        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/Room/CreateRoom.php",
-        params,
-        {
-          withCredentials: true,
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-        }
-      )
+      .post("http://localhost/API/Room/CreateRoom.php", params, {
+        withCredentials: true,
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
       .then((result) => {
-        console.log("--- 学習モード部屋の作成に成功 ---");
-        console.log("レスポンス: " + result.status);
+        console.log(result.status);
         params.delete("gamemode");
         history.push("/waiting");
       })
       .catch((error) => {
-        console.log("--- 学習モード部屋でエラーが発生 ---");
-        console.log("レスポンス: " + error.request.status);
+        console.log(error.request.status);
       });
   };
 
   const makeQuizRoom = () => {
     params.append("gamemode", quizMode);
     axios
-      .post(
-        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/Room/CreateRoom.php",
-        params,
-        {
-          withCredentials: true,
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-        }
-      )
+      .post("http://localhost/API/Room/CreateRoom.php", params, {
+        withCredentials: true,
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
       .then((result) => {
-        console.log("--- クイズモード部屋の作成に成功 ---");
-        console.log("レスポンス: " + result.status);
+        console.log(result.status);
         params.delete("gamemode");
         history.push("/waiting");
       })
       .catch((error) => {
-        console.log("--- クイズモード部屋でエラーが発生 ---");
-        console.log("レスポンス: " + error.request.status);
+        console.log(error.request.status);
       });
   };
 
@@ -103,25 +82,19 @@ export default function Selection() {
   const joinRoom = () => {
     params.append("roomID", roomId);
     axios
-      .post(
-        "http://localhost/~kubota/procon32_Lets_AIdea_php/API/Room/JoinRoom.php",
-        params,
-        {
-          withCredentials: true,
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-        }
-      )
+      .post("http://localhost/API/Room/JoinRoom.php", params, {
+        withCredentials: true,
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
       .then((result) => {
-        console.log("--- 部屋の参加に成功 ---");
         console.log(result);
         params.delete("roomID");
         history.push("/waiting");
       })
       .catch((error) => {
-        console.log("--- 部屋の参加に失敗 ---");
-        console.log("レスポンス: " + error.request.status);
+        console.log(error.request.status);
       });
   };
 
