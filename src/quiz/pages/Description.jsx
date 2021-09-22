75f961869ce3e3fbebc245df43931471b2ed78f2
ORIKAMI-NEP <ta.orikami@gmail.com>
2021-09-22 11:30:35 +0900

    Ctrl+R,F5,Form内Enterでのリロードを禁止から警告へ変更 #179

--- src/quiz/pages/Description.jsx
+++ src/quiz/pages/Description.jsx
@@ -72,17 +72,6 @@ export default function Description() {
       e.returnValue = "";
     };
     window.addEventListener("beforeunload", onUnload);
-    let key = false;
-    document.addEventListener("keydown", function (e) {
-      if (e.ctrlKey) key = true;
-      if (
-        ((e.which || e.keyCode) === 82 && key) ||
-        (e.which || e.keyCode) === 13 ||
-        (e.which || e.keyCode) === 116
-      ) {
-        e.preventDefault();
-      }
-    });
     window.history.pushState(null, null, location.href);
     window.addEventListener("popstate", (e) => {
       history.go(1);
